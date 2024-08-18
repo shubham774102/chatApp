3702faf60a319d9fb282ff45ede948d62127582b
shubham774102 <121854622+shubham774102@users.noreply.github.com>
2024-08-18 17:27:32 +0530

    icon update

--- Backend/Frontend/src/home/Rightpart/Chatuser.jsx
+++ Backend/Frontend/src/home/Rightpart/Chatuser.jsx
@@ -22,7 +22,7 @@ function Chatuser() {
       <div className="flex space-x-3 items-center justify-center h-[8vh] bg-gray-800 hover:bg-gray-700 duration-300">
         <div className="avatar online">
           <div className="w-16 rounded-full">
-            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src="https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-man-avatar-image-for-profile-png-image_9197911.png" />
           </div>
         </div>
         <div>
